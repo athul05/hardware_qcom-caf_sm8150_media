79170ad07b7c3881e90c6037a462f6de45d63791
Sagar Shah <sagarshah2326@gmail.com>
2020-04-01 03:30:29 +0530

    media: Refractor TARGET_BOARD_PLATFORM to PRODUCT_BOARD_PLATFORM
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- product.mk
+++ product.mk
@@ -23,7 +23,9 @@ MM_VIDEO += libstagefrighthw
 PRODUCT_PACKAGES += $(MM_VIDEO)
 
 ifeq ($(call is-board-platform-in-list, msmnile $(MSMSTEPPE) atoll $(TRINKET)),true)
-include hardware/qcom/media/conf_files/$(TARGET_BOARD_PLATFORM)/$(TARGET_BOARD_PLATFORM).mk
+include hardware/qcom/media/conf_files/$(PRODUCT_BOARD_PLATFORM)/$(PRODUCT_BOARD_PLATFORM).mk
+else ifeq ($(PRODUCT_BOARD_PLATFORM), sdmshrike)
+include hardware/qcom/media/conf_files/msmnile/msmnile.mk
 endif
 
 endif
